1a03f254cc37a4a3d0b68d09c96d512f2fa6874b
nicholae <nicholae@lab2-28.eng.utah.edu>
2018-11-21 18:22:47 -0700

    Nick added fully connected top module ready for testing, debugging, also passed through io between reg file and i2c

--- synth_top.v
+++ synth_top.v
@@ -1,6 +1,6 @@
 synth_top(
- input		  	vdd_io,	
- input		  	vss_io,	 
+ inout		  	vdd_pad_io,	
+ inout		  	vss_pad_io,	 
  input 		  	reset_io,
  input		  	clk_io,	 
  input [15:0] 	data_in_io,
@@ -30,6 +30,9 @@ wire [15:0]		core_to_r5;
 wire [15:0]		core_to_r6;
 wire [15:0]		core_to_r7;
 wire 			i2c_to_core_WE;
+wire [7:0]		i2c_to_core_data_wire;
+wire [7:0]		core_to_i2c_data_wire;
+wire [7:0]		i2c_to_core_reg_addr_wire;
 
 
 subcomponent subcomponent_instance_name (
@@ -43,6 +46,9 @@ Core c1(
   .core_to_mem_address			(addr_out_io),       	 // address of instruction
   .core_to_mem_write_enable		(core_to_mem_enable_io), 		 // memory write enable
   .i2c_sts						(i2c_sts_wire),
+  .i2c_to_core_data				(i2c_to_core_data_wire),    
+  .core_to_12c_data				(core_to_i2c_data_wire),      
+  .i2c_reg_addr					(i2c_to_core_reg_addr_wire),    
   .i2c_addr						(i2c_addr_wire),
   .pwm_reg0						(core_to_r0),
   .pwm_reg1						(core_to_r1),
@@ -55,14 +61,6 @@ Core c1(
 								/*			*/
   		);
 
-
-
-counter c1(	
-			.clk 				clk_io(),
-			.reset				(reset_io), 
-			.count_to			(),
-			.count      		()
-		  );
 
 pwm p0	  (	
 			.clk				(clk_io), 
@@ -125,13 +123,13 @@ module i2c_test(
 			.clock				(clk_io),
 			.reset				(reset_io),
 			.device_address		(i2c_addr_wire),
-			.reg_address		(),
-			.data_in			(),
+			.reg_address		(i2c_to_core_reg_addr_wire),
+			.data_in			(core_to_i2c_data_wire),
 	
-			.serial_data_line	(),
-			.serial_clock_line	(),
+			.serial_data_line	(sda_io),
+			.serial_clock_line	(scl_io),
 	
-			.data_out			(),
+			.data_out			(i2c_to_core_data_wire),
 			.i2c_status			(i2c_sts_wire),
 			.WE				    (i2c_to_core_WE)
 			);
